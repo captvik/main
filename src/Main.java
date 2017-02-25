e34a05bdbf7f8617aa95df3753481120021a099c
capt <vv@vv.ru>
2017-02-26 01:53:25 +0300

    3 задание

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,54 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream("input.txt")));
-        String s = in.readLine();
-            s = s.trim();
-            BigInteger a = BigInteger.valueOf(Integer.parseInt(s.substring(0, s.lastIndexOf(" "))));
-            BigInteger b = BigInteger.valueOf(Integer.parseInt(s.substring(s.lastIndexOf(" ") + 1, s.length())));
-            String text = (a.add(b.multiply(b))).toString();
-            PrintWriter out = new PrintWriter("output.txt");
-            out.println(text);
-            out.close();
+        int n = Integer.parseInt(in.readLine().trim());
+        String s = in.readLine().trim();
+        int [] mass = new int[n];
+        int [] mass_i = new int[n];
+        int index_1 = 0;
+        int index_2;
+
+        for (int k = 0; k < n; k++) {
+            if (k == n - 1) {
+                index_2 = s.length();
+            }
+            else {
+                index_2 = s.indexOf(" ", index_1 + 1);
+            }
+            mass[k] = Integer.parseInt(s.substring(index_1, index_2));
+            index_1 = index_2 + 1;
+        }
+
+        mass_i[0] = 1;
+
+        for (int j = 1; j < n; j++) {
+            int i = j - 1;
+
+            while (i >= 0 && mass[i] > mass[i + 1]) {
+                int temp = mass[i];
+                mass[i] = mass[i + 1];
+                mass[i + 1] = temp;
+                i--;
+            }
+            mass_i[j] = i + 2;
+        }
+
+        for (int i = 0; i < n; i++)
+            System.out.print(mass[i] + " ");
+        System.out.println();
+
+        System.out.println(n);
+        System.out.println(s);
+
+        String text_i = "";
+        String text_mass = "";
+        for (int i = 0; i < n; i++) {
+            text_i = text_i + mass_i[i] + " ";
+            text_mass = text_mass + mass[i] + " ";
+        }
+        PrintWriter out = new PrintWriter("output.txt");
+        out.println(text_i);
+        out.println(text_mass);
+        out.close();
     }
 }
