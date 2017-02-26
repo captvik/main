11e126d9cc97b4963bac188c069aac1219d8163a
capt <vv@vv.ru>
2017-02-26 23:31:38 +0300

    4 задание

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream("input.txt")));
         int n = Integer.parseInt(in.readLine().trim());
         String s = in.readLine().trim();
-        int [] mass = new int[n];
+        float [] mass = new float[n];
         int [] mass_i = new int[n];
         int index_1 = 0;
         int index_2;
@@ -19,32 +19,30 @@ public class Main {
             else {
                 index_2 = s.indexOf(" ", index_1 + 1);
             }
-            mass[k] = Integer.parseInt(s.substring(index_1, index_2));
+            mass[k] = Float.parseFloat(s.substring(index_1, index_2));
+            mass_i[k] = k + 1;
             index_1 = index_2 + 1;
         }
 
-        mass_i[0] = 1;
         for (int j = 1; j < n; j++) {  //алгоритм
             int i = j - 1;
 
             while (i >= 0 && mass[i] > mass[i + 1]) {
-                int temp = mass[i];
+                float temp = mass[i];
+                int temp_i = mass_i[i];
                 mass[i] = mass[i + 1];
+                mass_i[i] = mass_i[i + 1];
                 mass[i + 1] = temp;
+                mass_i[i + 1] = temp_i;
                 i--;
             }
-            mass_i[j] = i + 2;
         }
 
-        String text_i = "";            //вывод в файл
-        String text_mass = "";
-        for (int i = 0; i < n; i++) {
-            text_i = text_i + mass_i[i] + " ";
-            text_mass = text_mass + mass[i] + " ";
-        }
+        int sr = (mass_i.length - 1) / 2;
+        String text_i = mass_i[0] + " " + mass_i[sr] + " " + mass_i[mass_i.length - 1];
+
         PrintWriter out = new PrintWriter("output.txt");
         out.println(text_i);
-        out.println(text_mass);
         out.close();
     }
 }
