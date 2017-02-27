6f017019bf53a91463378c046ac766d97abf1420
capt <vv@vv.ru>
2017-02-27 20:10:11 +0300

    Секретарь Своп.

--- src/Main.java
+++ src/Main.java
@@ -22,19 +22,20 @@ public class Main {
             index_1 = index_2 + 1;
         }
         PrintWriter out = new PrintWriter("output.txt");
-        for (int j = 1; j < n; j++) {  //алгоритм
-            int i = j - 1;
+        for (int i = 0; i < n; i++) {  //алгоритм
+            int min = i;
             String text_i = "";
-            while (i >= 0 && mass[i] > mass[i + 1]) {
-                int temp = mass[i];
-                mass[i] = mass[i + 1];
-                mass[i + 1] = temp;
-                text_i = "Swap elements at indices " + (i + 1) + " and " + (i + 2) + ".";
-                out.println(text_i);
-                i--;
+            for (int j = min + 1; j < n; j++) {
+                if (mass[j] < mass[min])
+                    min = j;
             }
-            //if (i != j - 1)
-             //   out.println(text_i);
+            if (min != i) {
+                int temp = mass[i];
+                mass[i] = mass[min];
+                mass[min] = temp;
+                text_i = "Swap elements at indices " + (i + 1) + " and " + (min + 1) + ".";
+                out.println(text_i);
+            }
         }
         out.println("No more swaps needed.");
         String text = "";
